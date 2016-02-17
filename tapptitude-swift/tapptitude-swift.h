88b5fbd70032d3a3d6f09e993cc27e5464cf5a5f
Alexandru Tudose <alex.tudose@tapptitude.com>
2016-02-17 12:16:10 +0200

    Initial Commit

---
+++ tapptitude-swift/tapptitude-swift.h
@@ -0,0 +1,19 @@
+//
+//  tapptitude-swift.h
+//  tapptitude-swift
+//
+//  Created by Alexandru Tudose on 17/02/16.
+//  Copyright © 2016 Tapptitude. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for tapptitude-swift.
+FOUNDATION_EXPORT double tapptitude_swiftVersionNumber;
+
+//! Project version string for tapptitude-swift.
+FOUNDATION_EXPORT const unsigned char tapptitude_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tapptitude_swift/PublicHeader.h>
+
+
