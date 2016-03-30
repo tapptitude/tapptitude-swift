6d14addc8f727313df312bbd2878e02739ccbdd4
Alexandru Tudose <alex.tudose@tapptitude.com>
2016-03-30 18:08:08 +0300

    Tutorial update.

--- Playgrounds/Test.playground/Pages/Hybrid Controller & DataSource.xcplaygroundpage/Contents.swift
+++ Playgrounds/Test.playground/Pages/Hybrid Controller & DataSource.xcplaygroundpage/Contents.swift
@@ -24,7 +24,7 @@ class TextCell : UICollectionViewCell {
     }
 }
 
-
+// 1 cell controller --------
 let stringCellController = CollectionCellController<String, TextCell>(cellSize: CGSize(width: 50, height: 50))
 stringCellController.minimumInteritemSpacing = 20
 stringCellController.configureCell = { cell, content, indexPath in
@@ -35,6 +35,7 @@ stringCellController.didSelectContent = { content, indexPath, collectionView in
     print("did select", content)
 }
 
+// 2 cell controller --------
 let grayCellController = CollectionCellController<String, TextCell>(cellSize: CGSize(width: 50, height: 70))
 grayCellController.minimumInteritemSpacing = 20
 grayCellController.configureCell = { cell, content, indexPath in
@@ -46,7 +47,7 @@ grayCellController.didSelectContent = { content, indexPath, collectionView in
     print("did select", content)
 }
 
-
+// 3 cell controller --------
 let numberCellController = CollectionCellController<Int, UICollectionViewCell>(cellSize: CGSize(width: 100, height: 50))
 numberCellController.configureCell = { cell, content, indexPath in
     cell.backgroundColor = UIColor.blueColor()
@@ -56,7 +57,7 @@ numberCellController.didSelectContent = { content, indexPath, collectionView in
 }
 
 
-
+// datasource
 let dataSource = DataSource(["Maria", "232", 23])
 let multiCellController = HybridCellController([stringCellController, numberCellController, grayCellController])
 
