18db554e39fcd82638cff06fbea30ceea5fa41ff
Alexandru Tudose <alex.tudose@tapptitude.com>
2016-03-30 18:07:00 +0300

    Playground test -  hybrid data source - test.

--- Playgrounds/Test.playground/Pages/Hybrid Controller & DataSource.xcplaygroundpage/Contents.swift
+++ Playgrounds/Test.playground/Pages/Hybrid Controller & DataSource.xcplaygroundpage/Contents.swift
@@ -4,34 +4,67 @@ import UIKit
 import Tapptitude
 
 
-
-let firstController = CollectionCellController<String, UICollectionViewCell>(cellSize: CGSize(width: 50, height: 50))
-firstController.configureCell = {(cell, content, indexPath) in
-    print("firstController " + content)
+class TextCell : UICollectionViewCell {
+    var label: UILabel!
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        label = UILabel(frame: bounds)
+        label.textColor = UIColor.blackColor()
+        label.autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
+        label.textAlignment = .Center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(label)
+        
+        backgroundColor = UIColor(white: 0, alpha: 0.3)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
-let secondController = CollectionCellController<String, UICollectionViewCell>(cellSize: CGSize(width: 50, height: 50))
-secondController.configureCell = {(cell, content, indexPath) in
-    print("secondController " + content)
+
+let stringCellController = CollectionCellController<String, TextCell>(cellSize: CGSize(width: 50, height: 50))
+stringCellController.minimumInteritemSpacing = 20
+stringCellController.configureCell = { cell, content, indexPath in
+    cell.backgroundColor = UIColor.redColor()
+    cell.label.text = content
+}
+stringCellController.didSelectContent = { content, indexPath, collectionView in
+    print("did select", content)
 }
 
-let content = NSArray(arrayLiteral: "2")
-let cellController = HybridCellController([firstController, secondController])
-var dataSource = HybridDataSource(content: ["2"], multiCellController: cellController)
-
-var indexPath = NSIndexPath(forItem: 0, inSection: 0)
-let item = dataSource.objectAtIndexPath(indexPath)
-let collectionView = UICollectionView(frame: CGRectZero, collectionViewLayout: UICollectionViewFlowLayout())
-
-cellController.configureCell(UICollectionViewCell(), forContent: item, indexPath: indexPath)
-cellController.minimumLineSpacingForContent(item, collectionView: collectionView)
-cellController.minimumLineSpacingForContent(item, collectionView: collectionView)
-
-indexPath = NSIndexPath(forItem: 1, inSection: 0)
-cellController.configureCell(UICollectionViewCell(), forContent: dataSource.objectAtIndexPath(indexPath), indexPath: indexPath)
-
-cellController.configureCell(UICollectionViewCell(), forContent: "4", indexPath: indexPath)
+let grayCellController = CollectionCellController<String, TextCell>(cellSize: CGSize(width: 50, height: 70))
+grayCellController.minimumInteritemSpacing = 20
+grayCellController.configureCell = { cell, content, indexPath in
+    cell.backgroundColor = UIColor.grayColor()
+    cell.label.textColor = UIColor.whiteColor()
+    cell.label.text = content
+}
+grayCellController.didSelectContent = { content, indexPath, collectionView in
+    print("did select", content)
+}
 
 
+let numberCellController = CollectionCellController<Int, UICollectionViewCell>(cellSize: CGSize(width: 100, height: 50))
+numberCellController.configureCell = { cell, content, indexPath in
+    cell.backgroundColor = UIColor.blueColor()
+}
+numberCellController.didSelectContent = { content, indexPath, collectionView in
+    print("did select", content)
+}
+
+
+
+let dataSource = DataSource(["Maria", "232", 23])
+let multiCellController = HybridCellController([stringCellController, numberCellController, grayCellController])
+
+let feedController = CollectionFeedController()
+feedController.dataSource = HybridDataSource(content: ["Maria", "232", 23], multiCellController: multiCellController)
+feedController.cellController = multiCellController
+
+import XCPlayground
+XCPlaygroundPage.currentPage.liveView = feedController.view
 
 //: [Next](@next)
