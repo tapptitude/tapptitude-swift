9cc3d669ad4f2fdfa56638907cd7a513ad450c23
Alexandru Tudose <alex.tudose@tapptitude.com>
2017-02-23 18:25:20 +0200

    Keyboard controller improvements.

--- Tapptitude/Helpers/KeyboardVisibilityController.swift
+++ Tapptitude/Helpers/KeyboardVisibilityController.swift
@@ -11,8 +11,10 @@ import UIKit
 
 public class KeyboardVisibilityController: NSObject {
     
-    open weak var view: UIView? // this view will be translated up, will make firstResponder visible
-    open weak var toBeVisibleView: UIView? //when keyboard is visible move this view up
+    /// this view will be translated up, will make firstResponder visible
+    open weak var view: UIView?
+    ///when keyboard is visible move this view up, toBeVisibleView == nil --> == firstResponderView
+    open weak var toBeVisibleView: UIView?
     
     open var dismissKeyboardTouchRecognizer: TouchRecognizer? { //nil by default
         willSet {
@@ -21,11 +23,14 @@ public class KeyboardVisibilityController: NSObject {
             }
         }
     }
-    open var moveViewUpByValue: CGFloat = 0 // move by a exact value, when 0 view is moved up by keyboard height
-    open var addMoveUpValue: CGFloat = 0 // you can add extra height
-    open var makeFirstRespondeSuperviewVisible: Bool = false //instead of firstResponder view
     
-    open var additionallAnimatioBlock: ((_ moveUpValue: CGFloat) -> Void)? //view properties to be animated, 0 when returning to original value
+    /// you can add extra space between keyboard and toBeVisibleView
+    open var extraSpaceAboveKeyboard: CGFloat = 0
+    /// instead of firstResponder view
+    open var makeFirstRespondeSuperviewVisible: Bool = false
+    
+    ///view properties to be animated, 0 when returning to original value
+    open var additionallAnimatioBlock: ((_ moveUpValue: CGFloat) -> Void)?
     open var disableKeyboardMoveUpAnimation: Bool = false
     open var isKeyboardVisible: Bool = false
     open var applyTransformToVisibleView: Bool = true
@@ -34,24 +39,24 @@ public class KeyboardVisibilityController: NSObject {
         super.init()
         
         NotificationCenter.default.addObserver(self,
-                                                         selector: #selector(KeyboardVisibilityController.keyboardWillAppear),
-                                                         name: NSNotification.Name.UIKeyboardWillShow,
-                                                         object: nil)
+                                               selector: #selector(keyboardWillAppear),
+                                               name: .UIKeyboardWillShow,
+                                               object: nil)
         
         NotificationCenter.default.addObserver(self,
-                                                         selector: #selector(KeyboardVisibilityController.keyboardWillDisappear),
-                                                         name: NSNotification.Name.UIKeyboardWillHide,
-                                                         object: nil)
+                                               selector: #selector(keyboardWillDisappear),
+                                               name: .UIKeyboardWillHide,
+                                               object: nil)
         
         NotificationCenter.default.addObserver(self,
-                                                         selector: #selector(KeyboardVisibilityController.applicationWillResign),
-                                                         name: NSNotification.Name.UIApplicationWillResignActive,
-                                                         object: nil)
+                                               selector: #selector(applicationWillResign),
+                                               name: .UIApplicationWillResignActive,
+                                               object: nil)
         
         NotificationCenter.default.addObserver(self,
-                                                         selector: #selector(KeyboardVisibilityController.keyboardWillChangeFrame),
-                                                         name: NSNotification.Name.UIKeyboardWillChangeFrame,
-                                                         object: nil)
+                                               selector: #selector(keyboardWillChangeFrame),
+                                               name: .UIKeyboardWillChangeFrame,
+                                               object: nil)
     }
     
     convenience public init(viewToMove moveView: UIView) {
@@ -68,17 +73,15 @@ public class KeyboardVisibilityController: NSObject {
     }
     
     //MARK: - keyboard events
-    
+    fileprivate var scrollViewPreviousBottomInset: CGFloat = 0
     func keyboardWillAppear(notification: Notification) {
         self.isKeyboardVisible = true
         self.dismissKeyboardTouchRecognizer?.isEnabled = true
-//        self.moveViewUp(true, usingKeyboardNotification: notification)
     }
     
     func keyboardWillDisappear(notification: Notification) {
         self.isKeyboardVisible = false
         self.dismissKeyboardTouchRecognizer?.isEnabled = false
-//        self.moveViewUp(false, usingKeyboardNotification: notification)
     }
     
     func applicationWillResign(notification: Notification) {
@@ -92,7 +95,7 @@ public class KeyboardVisibilityController: NSObject {
     }
     
     func moveViewUp(up: Bool, usingKeyboardNotification notification: Notification) {
-        if self.view == nil || self.view?.window == nil {
+        guard let toMoveView = self.view, let window = toMoveView.window else {
             return //ingore
         }
         
@@ -102,7 +105,7 @@ public class KeyboardVisibilityController: NSObject {
         var toBeVisibleView = self.toBeVisibleView
         
         if toBeVisibleView == nil {
-            toBeVisibleView = self.view?.findFirstResponder()
+            toBeVisibleView = toMoveView.findFirstResponder()
             if self.makeFirstRespondeSuperviewVisible {
                 toBeVisibleView = toBeVisibleView?.superview
             }
@@ -122,55 +125,48 @@ public class KeyboardVisibilityController: NSObject {
             UIView.setAnimationBeginsFromCurrentState(true)
         }
         
-        var moveUpValue: CGFloat = self.moveViewUpByValue
-        if let visibleView = toBeVisibleView, let endFrame = keyboardEndFrame {
-            let frame = visibleView.convert(visibleView.bounds, to: visibleView.window)
-            let deltaY = frame.maxY - endFrame.origin.y
+        var moveUpValue: CGFloat = 0
+        if let visibleView = toBeVisibleView, let keyboardEndFrame = keyboardEndFrame, visibleView.window == window {
+            let frame = visibleView.superview!.convert(visibleView.frame, to: visibleView.window)
+            let deltaY = frame.maxY - (keyboardEndFrame.origin.y - extraSpaceAboveKeyboard) + (-toMoveView.transform.ty)
             
             if applyTransformToVisibleView {
-                let shouldMove = (deltaY - self.view!.transform.ty) > 0
-                if shouldMove {
-                    moveUpValue = deltaY - self.view!.transform.ty
-                }
-                moveUpValue += addMoveUpValue
+                let shouldMove = deltaY > 0
+                moveUpValue += shouldMove ? deltaY : moveUpValue
             } else {
                 let key = "previousMoveUpValue"
-                let previousMoveUpValue = (self.view?.layer.value(forKey: key) as? CGFloat) ?? 0
-                moveUpValue = deltaY + previousMoveUpValue
-                moveUpValue += addMoveUpValue
-                self.view?.layer.setValue(up ? moveUpValue : 0, forKey: key)
+                let previousMoveUpValue = (toMoveView.layer.value(forKey: key) as? CGFloat) ?? 0
+                moveUpValue += deltaY + previousMoveUpValue
+                toMoveView.layer.setValue(up ? moveUpValue : 0, forKey: key)
             }
         }
         
-        if let scrollView = self.view as? UIScrollView {
-            let frame = scrollView.convert(scrollView.bounds, to: scrollView.window)
-            let diff = scrollView.window!.bounds.size.height - frame.maxY
+        if let scrollView = toMoveView as? UIScrollView {
+            // calculate scrollview bottom inset
+            let scrollWindowframe = scrollView.superview!.convert(scrollView.frame, to: window)
+            let intersectKeyboardHeight = scrollWindowframe.intersection(keyboardEndFrame!).height
+            let bottomInsetMoveUpValue = intersectKeyboardHeight - scrollViewPreviousBottomInset
+            let insetBottom = scrollView.contentInset.bottom + bottomInsetMoveUpValue
+            scrollViewPreviousBottomInset = intersectKeyboardHeight
             
-            let key = "previousInsetBottom"
-            var inset = scrollView.contentInset
-            if up {
-                if scrollView.layer.value(forKey: key) == nil {
-                    scrollView.layer.setValue(inset.bottom, forKey: key)
-                }
-                inset.bottom = keyboardEndFrame!.size.height - diff
-            } else {
-                if let bottom = scrollView.layer.value(forKey: key) as? CGFloat {
-                    inset.bottom = bottom
-                }
-            }
             
-            scrollView.contentInset = inset
-            var indicatorInset = scrollView.scrollIndicatorInsets
-            indicatorInset.bottom = inset.bottom
-            scrollView.scrollIndicatorInsets = indicatorInset
+            scrollView.contentInset.bottom = insetBottom
+            scrollView.scrollIndicatorInsets.bottom = insetBottom
             
-            if up {
-                let newY = scrollView.contentOffset.y + (up ? moveUpValue : 0)
-                scrollView.contentOffset = CGPoint(x: 0, y: newY)
+            let isInputAccessoryView = toBeVisibleView?.window == nil || toBeVisibleView?.window != scrollView.window
+            
+            // move up content offset - so they can be in sync
+            if moveUpValue > 0 {
+                scrollView.contentOffset.y += moveUpValue
+            } else if bottomInsetMoveUpValue > 0, isInputAccessoryView {
+                let availableYSpace = (scrollView.contentSize.height + scrollViewPreviousBottomInset) - scrollView.bounds.height
+                moveUpValue = min(bottomInsetMoveUpValue, max(0, availableYSpace))
+                moveUpValue += extraSpaceAboveKeyboard
+                scrollView.contentOffset.y += moveUpValue
             }
         } else {
             if applyTransformToVisibleView {
-                self.view?.transform = up ? CGAffineTransform(translationX: 0, y: -moveUpValue) : CGAffineTransform.identity
+                toMoveView.transform = up ? CGAffineTransform(translationX: 0, y: -moveUpValue) : CGAffineTransform.identity
             }
         }
         
